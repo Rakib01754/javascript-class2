aafa582190009835bc2565c3cabd1b37764f79bd
Rakib01754 <rakibul.freelancer567@gmail.com>
2022-08-01 23:22:02 +0600

    cocnditional added

---
+++ conditional.js
@@ -0,0 +1,37 @@
+
+// // if condition //
+// var carPrice = 500000;
+// var myCash = 70000;
+
+// if (carPrice > myCash) {
+//     console.log('Basai Fire Jabo');
+// }
+
+// var carPrice = 500000;
+// var myCash = 70000;
+
+// if (carPrice > myCash) {
+//     console.log('Basai Fire Jabo');
+// }
+
+// if and else condition 
+
+// var iphonePrice = 120000;
+// var myBudget = 150000;
+
+// if (iphonePrice < myBudget) {
+//     console.log('i will buy iphone')
+// }
+// else {
+//     console.log('Basai Jaiya muri khamu');
+// }
+
+var rickshaVara = 50;
+var pocketMoney = 70;
+
+if (rickshaVara >= pocketMoney) {
+    console.log('ami hete hete basai jaabo')
+}
+else {
+    console.log('Rickshaw diye aramse jabo')
+}

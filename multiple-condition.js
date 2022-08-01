cca2326b5ebc07bab74517c33d6a3c4e12480301
Rakib01754 <rakibul.freelancer567@gmail.com>
2022-08-02 01:04:06 +0600

    multiple added

---
+++ multiple-condition.js
@@ -0,0 +1,23 @@
+// var isBcsCadre = true;
+// var salary = 30000;
+// var nickName = 'rakib';
+// var car = 1;
+
+
+// if (isBcsCadre === true && salary > 40000 && nickName == 'rakib' && car < 1) {
+//     console.log('Biye Final');
+// }
+// else {
+//     console.log('Sob kichu ready kore tarpor biye korte jaiyo');
+// }
+
+// var isEngineer = true;
+// var incomeMonthly = 50000;
+// var properties = true;
+
+// if (isEngineer == true || incomeMonthly > 80000 || properties == true) {
+//     console.log('Biyer Bebosta Hoye Geche');
+// }
+// else {
+//     console.log('Tumar mone hoy r biye kora hobena');
+// }

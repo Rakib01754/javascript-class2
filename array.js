2f0cc567ff21b7ea797fd9d7c2ba32762147dcb4
Rakib01754 <rakibul.freelancer567@gmail.com>
2022-08-01 10:45:52 +0600

    array and index js added

---
+++ array.js
@@ -0,0 +1,9 @@
+// array declaration system 
+
+var favouriteColors = ['red', 'green', 'blue', 'pink', 'tomato'];
+
+var playersScore = [10, 20, 80, 100, 45, 60];
+
+console.log(playersScore);
+
+console.log(favouriteColors.length);
---
+++ index.js
@@ -0,0 +1,73 @@
+var namesOfFriends = ['rakib', 'rana', 'arif', 'sajib', 'ratul', 'rafi']
+
+// get elements by index //
+
+var justFriend = namesOfFriends[3];
+
+console.log(justFriend);
+
+
+// set element by index //
+
+namesOfFriends[5] = 'papon';
+console.log(namesOfFriends);
+
+// find element by index  //
+
+console.log(namesOfFriends.indexOf('arif'));
+
+
+
+// -------------------another try for better understanding------------------------------ //
+
+var nameOfCountries = ['bangladesh', 'india', 'pakistan', 'china', 'nepal', 'bhutan'];
+
+// get elements by index //
+
+var powefulCountry = nameOfCountries[3];
+
+console.log(powefulCountry);
+
+
+// set element by index //
+
+nameOfCountries[5] = 'maldives';
+
+console.log(nameOfCountries);
+
+
+// find element position by index //
+
+
+var indexPositionOfNepal = nameOfCountries.indexOf('nepal');
+
+console.log(indexPositionOfNepal);
+
+
+//------------------- one more try for better understanding--------------------- //
+
+var marks = [10, 52, 43, 46, 85, 78, 96, 83]
+
+
+
+// get element by index //
+
+var banglaMarks = marks[5];
+
+console.log(banglaMarks);
+
+
+// set element by index //
+
+marks[6] = 98;
+
+console.log(marks);
+
+
+// find position by index //
+
+var positionOfBangla = marks.indexOf(78);
+
+console.log(positionOfBangla);
+
+

0bcc562979da20546aa85d62f886b4ba753b7465
Rakib01754 <rakibul.freelancer567@gmail.com>
2022-08-02 01:23:23 +0600

    multi stage added

---
+++ multi-stage.js
@@ -0,0 +1,21 @@
+var cash = 200;
+var pizza = 180;
+var burger = 120;
+var bread = 30;
+
+
+if (cash > pizza) {
+    console.log('pizza kine khabo');
+}
+else if (cash > burger) {
+    console.log('burger kine khabo');
+}
+else if (cash > bread) {
+    console.log('ruti kine khaboo');
+}
+else {
+    console.log('na kheye bari chole jabo');
+}
+
+
+
